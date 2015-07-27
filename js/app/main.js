98cec1095563fbc0705a7913fc6e32e5708c3668
senyawang <senyawang@sina.cn>
2015-07-27 23:38:43 +0800

    add

--- js/app/main.js
+++ js/app/main.js
@@ -1,24 +1,6 @@
 $(function () {
 
-	$('#panel').panel({
-        contentWrap: $('.mainpage')
-    });
-	$('#push-right').on('touchend', function () {
-        $('#panel').panel('toggle', 'push', 'right');
-    });
-
-    $("#slider").show();
-    $("#slider").slider({
-        loop: true,
-        autoPlay: true,
-        dots: false,
-        arrow: false,
-        imgZoom: true
-    });
-
-    $('#nav').navigator();
-
-    function refreshInit (url) {
+	function refreshInit (url) {
     	$('.ui-refresh').refresh({
 	        load: function (dir, type) {
 	            var me = this;
@@ -39,7 +21,30 @@ $(function () {
 	    });
     }
 
-    refreshInit("url")
+    refreshInit("url");
+
+	var uiMask = $('<div class="ui-masks"></div>');
+
+	$('#panel').panel({
+        contentWrap: $('.mainpage')
+    });
+	$('#push-right').on('touchend', function () {
+        $('#panel').panel('toggle', 'overlay', 'right');
+        // uiMask
+        // 	.appendTo("#page")
+        // 	.css('opacity',0.5);
+    });
+
+    $("#slider").show();
+    $("#slider").slider({
+        loop: true,
+        autoPlay: true,
+        dots: false,
+        arrow: false,
+        imgZoom: true
+    });
+
+    $('#nav').navigator();
 
     $(".slide-panel").on('touchend', '.current', function (e) {
 
@@ -48,8 +53,38 @@ $(function () {
     	var $this = $(this);
 
     	$this.css('marginLeft', '-50%');
+    });
+
+    $('#searchForm').on('input', '[type="search"]', function (e) {
+    	
+    	var $parent = $(this).parents('#searchForm');
+
+    	$parent.find('.s-btn').show();
+
+    	$parent.find('.sf-wrap').css({'text-align': 'left'});
+
     })
 
+    $('.J_dropMenu').on('tap', function (e) {
+
+    	e.preventDefault();
+
+    	$(this)
+    		.toggleClass('current')
+    		.siblings()
+    		.removeClass('current');
+
+    	$('.s-mask').show();
+    	$('.J_dropCont').show();	
+    })
+
+    $(document).on('tap', '.s-mask', function (e) {
+    	
+    	$(this).hide();
+    	$('.J_dropCont').hide();
+    	$('.J_dropMenu').removeClass('current');
+
+    })
 
 
 
